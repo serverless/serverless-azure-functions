85006603ad1907ff5d125adc615f3f2f07a6ee7f
Pragna Gopa <pgopa@microsoft.com>
2017-02-24 11:39:45 -0800

    Simplify zip and upload function code (#27)
    * use zip-folder package instead of js-zip

--- deploy/lib/createFunctions.js
+++ deploy/lib/createFunctions.js
@@ -10,11 +10,10 @@ module.exports = {
     this.serverless.service.getAllFunctions().forEach((functionName) => {
       const metaData = utils.getFunctionMetaData(functionName, this.provider.getParsedBindings(), this.serverless);
 
-      createFunctionPromises.push(this.provider.createZipObject(functionName, metaData.entryPoint, metaData.handlerPath, metaData.params));
+      createFunctionPromises.push(this.provider.createZipObjectAndUploadFunction(functionName, metaData.entryPoint, metaData.handlerPath, metaData.params));
     });
 
     return BbPromise.all(createFunctionPromises)
-            .then(() => this.provider.createAndUploadZipFunctions())
             .then(() => this.provider.syncTriggers())
             .then(() => this.provider.runKuduCommand('del package.json'))
             .then(() => this.provider.uploadPackageJson())
