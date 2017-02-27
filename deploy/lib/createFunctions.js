86526ed627655972fb7354a7c3dfddc8458fa9bc
Pragna Gopa <pgopa@microsoft.com>
2017-02-27 11:38:42 -0800

    Fixes #29 and fix request URL for uploading functionName.zip file (#31)

--- deploy/lib/createFunctions.js
+++ deploy/lib/createFunctions.js
@@ -2,6 +2,8 @@
 
 const BbPromise = require('bluebird');
 const utils = require('../../shared/utils');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   createFunctions () {
@@ -12,11 +14,18 @@ module.exports = {
 
       createFunctionPromises.push(this.provider.createZipObjectAndUploadFunction(functionName, metaData.entryPoint, metaData.handlerPath, metaData.params));
     });
-
-    return BbPromise.all(createFunctionPromises)
-            .then(() => this.provider.syncTriggers())
-            .then(() => this.provider.runKuduCommand('del package.json'))
-            .then(() => this.provider.uploadPackageJson())
+    
+    const packageJsonFilePath = path.join(this.serverless.config.servicePath, 'package.json');
+    let createFunctionsPromise = BbPromise.all(createFunctionPromises)
+                                .then(() => this.provider.syncTriggers())
+                                .then(() => this.provider.runKuduCommand('del package.json'));
+   
+    if (fs.existsSync(packageJsonFilePath)) {
+      return createFunctionsPromise.then(() => this.provider.uploadPackageJson())
             .then(() => this.provider.runKuduCommand('npm install --production'));
+    }
+    else{
+      return createFunctionsPromise.then(() => this.provider.runKuduCommand('rmdir /s /q node_modules'))
+    }
   }
 };
