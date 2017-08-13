773f6113da9798c812513b2c81781196b72aa714
Taras <trane9991@gmail.com>
2017-08-14 00:19:56 +0300

    remove running "npm install" with Kudo, now dependencies can be supplied with node_modules

--- deploy/lib/createFunctions.js
+++ deploy/lib/createFunctions.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const BbPromise = require('bluebird');
-const fs = require('fs');
 const path = require('path');
 
 module.exports = {
@@ -14,17 +13,7 @@ module.exports = {
         .then(() => this.provider.runKuduCommand('mv '+ path.join(functionName, functionName +'-function.json') +' '+ path.join(functionName, 'function.json'))));
     });
 
-    const packageJsonFilePath = path.join(this.serverless.config.servicePath, 'package.json');
-    let createFunctionsPromise = BbPromise.all(createFunctionPromises)
-                                .then(() => this.provider.syncTriggers())
-                                .then(() => this.provider.runKuduCommand('del package.json'));
-
-    if (fs.existsSync(packageJsonFilePath)) {
-      return createFunctionsPromise.then(() => this.provider.uploadPackageJson())
-            .then(() => this.provider.runKuduCommand('npm install --production'));
-    }
-    else {
-      return createFunctionsPromise.then(() => this.provider.runKuduCommand('rmdir /s /q node_modules'));
-    }
+    return BbPromise.all(createFunctionPromises)
+            .then(() => this.provider.syncTriggers());
   }
 };
