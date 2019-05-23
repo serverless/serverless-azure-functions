0f89cb633efadc8f87e90dae443eed3cb3ccb8a5
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-05-23 11:19:21 -0700

    feat: Mock of Serverless object for unit tests (#144)
    Resolves [AB#18712]

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testRegex: "((\\.|/)(test|spec))\\.ts$",
+  moduleFileExtensions:  ["ts", "tsx", "js"],
   testPathIgnorePatterns: [
     "/lib/",
     "/node_modules/"
---
+++ src/test/mockFactory.ts
@@ -0,0 +1,80 @@
+import Serverless from 'serverless';
+import Service from 'serverless/classes/Service';
+import Utils = require('serverless/classes/Utils');
+import PluginManager = require('serverless/classes/PluginManager');
+
+export class MockFactory {
+  public static createTestServerless(config?: any): Serverless {
+    const sls = new Serverless(config);
+    sls.service = MockFactory.createTestService();
+    sls.utils = MockFactory.createTestUtils();
+    sls.cli = MockFactory.createTestCli();
+    sls.pluginManager = MockFactory.createTestPluginManager();
+    return sls;
+  }
+
+  private static createTestService(): Service {
+    return {
+      getAllFunctions: jest.fn(),
+      getFunction: jest.fn(),
+      getAllEventsInFunction: jest.fn(),
+      getAllFunctionsNames: jest.fn(),
+      getEventInFunction: jest.fn(),
+      getServiceName: jest.fn(),
+      load: jest.fn(),
+      mergeResourceArrays: jest.fn(),
+      setFunctionNames: jest.fn(),
+      update: jest.fn(),
+      validate: jest.fn(),
+      custom: null,
+      provider: null
+    };
+  }
+
+  private static createTestUtils(): Utils {
+    return {
+      appendFileSync: jest.fn(),
+      copyDirContentsSync: jest.fn(),
+      dirExistsSync: jest.fn(),
+      fileExistsSync: jest.fn(),
+      findServicePath: jest.fn(),
+      generateShortId: jest.fn(),
+      getVersion: jest.fn(),
+      logStat: jest.fn(),
+      readFile: jest.fn(),
+      readFileSync: jest.fn(),
+      walkDirSync: jest.fn(),
+      writeFile: jest.fn(),
+      writeFileDir: jest.fn(),
+      writeFileSync: jest.fn()
+    }
+  }
+
+  private static createTestCli(){
+    return {
+      log: jest.fn()
+    }
+  }
+
+  private static createTestPluginManager(): PluginManager {
+    return  {
+      addPlugin: jest.fn(),
+      cliCommands: null,
+      cliOptions: null,
+      commands: null,
+      deprecatedEvents: null,
+      hooks: null,
+      loadAllPlugins: jest.fn(),
+      loadCommand: jest.fn(),
+      loadCommands: jest.fn(),
+      loadCorePlugins: jest.fn(),
+      loadPlugins: jest.fn(),
+      loadServicePlugins: jest.fn(),
+      plugins: null,
+      serverless: null,
+      setCliCommands: jest.fn(),
+      setCliOptions: jest.fn(),
+      spawn: jest.fn()
+    }
+  }
+}
