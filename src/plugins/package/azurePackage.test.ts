854a533ecb3d215ace2a983393869e53a347a50a
Wallace Breza <wallace@breza.me>
2019-06-11 17:46:21 -0700

    test: Verify package service is called as expected during packing process

--- src/plugins/package/azurePackage.test.ts
+++ src/plugins/package/azurePackage.test.ts
@@ -1,20 +1,43 @@
+import Serverless from "serverless";
 import { MockFactory } from "../../test/mockFactory";
 import { invokeHook } from "../../test/utils";
 import { AzurePackage } from "./azurePackage";
+jest.mock("../../services/packageService");
 import { PackageService } from "../../services/packageService";
 
 describe("Azure Package Plugin", () => {
-  it("sets up provider configuration", async () => {
+  let sls: Serverless;
+  let plugin: AzurePackage;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
     const slsFunctionConfig = MockFactory.createTestSlsFunctionConfig();
-    const sls = MockFactory.createTestServerless();
+    sls = MockFactory.createTestServerless();
 
-    PackageService.prototype.createBindings = jest.fn(() => Promise.resolve([]));
+    plugin = new AzurePackage(sls);
+  });
 
-    const options = MockFactory.createTestServerlessOptions();
-    const plugin = new AzurePackage(sls, options);
+  it("sets creates function bindings before package:setupProviderConfiguration life cycle event", async () => {
     await invokeHook(plugin, "before:package:setupProviderConfiguration");
-
     expect(PackageService.prototype.createBindings).toBeCalled();
   });
 
+  it("prepares the package for webpack before webpack:package:packageModules life cycle event", async () => {
+    await invokeHook(plugin, "before:webpack:package:packageModules");
+    expect(PackageService.prototype.createBindings).toBeCalled();
+    expect(PackageService.prototype.prepareWebpack).toBeCalled();
+  });
+
+  it("only calls create bindings 1 time throughout full package life cycle", async () => {
+    await invokeHook(plugin, "before:package:setupProviderConfiguration");
+    await invokeHook(plugin, "before:webpack:package:packageModules");
+
+    expect(PackageService.prototype.createBindings).toBeCalledTimes(1);
+    expect(PackageService.prototype.prepareWebpack).toBeCalledTimes(1);
+  });
+
+  it("cleans up package after package:finalize", async () => {
+    await invokeHook(plugin, "after:package:finalize");
+    expect(PackageService.prototype.cleanUp).toBeCalled();
+  })
 });
--- src/plugins/package/azurePackage.ts
+++ src/plugins/package/azurePackage.ts
@@ -9,7 +9,7 @@ export class AzurePackage {
   public provider: AzureProvider;
   public hooks: { [eventName: string]: Promise<any> };
 
-  public constructor(private serverless: Serverless, private options: Serverless.Options) {
+  public constructor(private serverless: Serverless) {
     this.hooks = {
       "before:package:setupProviderConfiguration": this.setupProviderConfiguration.bind(this),
       "before:webpack:package:packageModules": this.webpack.bind(this),
@@ -19,14 +19,16 @@ export class AzurePackage {
     this.packageService = new PackageService(this.serverless);
   }
 
-  private async setupProviderConfiguration(): Promise<any[]> {
-    return await this.packageService.createBindings();
+  private async setupProviderConfiguration(): Promise<void> {
+    await this.packageService.createBindings();
+    this.bindingsCreated = true;
+
+    return Promise.resolve();
   }
 
   private async webpack(): Promise<void> {
-
     if (!this.bindingsCreated) {
-      await this.packageService.createBindings();
+      await this.setupProviderConfiguration();
     }
 
     await this.packageService.prepareWebpack();
