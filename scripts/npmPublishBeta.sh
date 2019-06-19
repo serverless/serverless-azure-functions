79ebab45f9ec1bbec7e020ff8ec7df8d5b8eaaa5
My <mydiemho@users.noreply.github.com>
2019-06-19 09:58:15 -0700

    [CI/CD] build to release npm beta packages (#167)
    * update travis.yml

---
+++ scripts/npmPublishBeta.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+echo "This script will bump npm version and push changes to new branch"
+
+# echo "checkout new branch for version bump"
+git checkout -b npmRelease
+
+## configure npm to sign commit
+npm config set sign-git-tag true
+
+version=$(npm version prerelease -m "Bumped to version %s")
+
+echo "Bumped to version ${version}"
+
+# remove git tag, we don't want to tag pr
+git tag -d ${version} 
+
+# push to remote
+git push origin npmRelease
+
+## only for testing CD on test branch
+## after development, we push tag on dev/master manually
+# git push origin ${version}
