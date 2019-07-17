7ceb4848ca381ee3cbb5dbbe67e88f876a0b14eb
njm3754 <32784910+njm3754@users.noreply.github.com>
2019-07-17 09:34:56 -0700

    feat: Added feature to specify Azure subcription ID in CLI before deployment (#203)
    * option to specify subscription id from CLI added
    
    * Build issue resolved
    
    * azureBasePlugin issue resolved
    
    * Two test cases added to test subscription ID feature
    
    * Changes made accoridng to feedback

--- src/plugins/login/azureLoginPlugin.ts
+++ src/plugins/login/azureLoginPlugin.ts
@@ -2,12 +2,13 @@ import Serverless from "serverless";
 import AzureProvider from "../../provider/azureProvider";
 import { AzureLoginService } from "../../services/loginService";
 import { AzureBasePlugin } from "../azureBasePlugin";
+import { AzureLoginOptions } from "../../services/loginService";
 
-export class AzureLoginPlugin extends AzureBasePlugin {
+export class AzureLoginPlugin extends AzureBasePlugin<AzureLoginOptions> {
   private provider: AzureProvider;
   public hooks: { [eventName: string]: Promise<any> };
 
-  public constructor(serverless: Serverless, options: Serverless.Options) {
+  public constructor(serverless: Serverless, options: AzureLoginOptions) {
     super(serverless, options);
     this.provider = (this.serverless.getProvider("azure") as any) as AzureProvider;
 
@@ -32,7 +33,8 @@ export class AzureLoginPlugin extends AzureBasePlugin {
       this.serverless.variables["azureCredentials"] = authResult.credentials;
       // Use environment variable for sub ID or use the first subscription in the list (service principal can
       // have access to more than one subscription)
-      this.serverless.variables["subscriptionId"] = process.env.azureSubId || authResult.subscriptions[0].id;
+      this.serverless.variables["subscriptionId"] = this.options.subscriptionId || process.env.azureSubId || authResult.subscriptions[0].id;
+      this.serverless.cli.log(`Using subscription ID: ${this.serverless.variables["subscriptionId"]}`);
     }
     catch (e) {
       this.log("Error logging into azure");
