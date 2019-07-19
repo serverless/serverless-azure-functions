15d2980f0c73dd304c3f82903501e0e67430692f
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-07-19 08:28:15 -0700

    feat: Deploy pre-existing package with `-p` CLI option (#205)
    - [x] Allow `-p` or `--package` param to specify package to deploy
    - [x] Log in before deployment rather than packaging
    - [x] Don't run deployment if local package does not exist
    
    Resolves [AB#500]

--- src/plugins/package/azurePackagePlugin.test.ts
+++ src/plugins/package/azurePackagePlugin.test.ts
@@ -39,5 +39,33 @@ describe("Azure Package Plugin", () => {
   it("cleans up package after package:finalize", async () => {
     await invokeHook(plugin, "after:package:finalize");
     expect(PackageService.prototype.cleanUp).toBeCalled();
+  });
+
+  describe("Package specified in options", () => {
+
+    beforeEach(() => {
+      plugin = new AzurePackagePlugin(sls, MockFactory.createTestServerlessOptions({
+        package: "fake.zip",
+      }));
+    });
+
+    it("does not call create bindings if package specified in options", async () => {
+      await invokeHook(plugin, "before:package:setupProviderConfiguration");
+      expect(PackageService.prototype.createBindings).not.toBeCalled();
+      expect(sls.cli.log).lastCalledWith("No need to create bindings. Using pre-existing package");
+    });
+
+    it("does not call webpack if package specified in options", async () => {
+      await invokeHook(plugin, "before:webpack:package:packageModules");
+      expect(PackageService.prototype.createBindings).not.toBeCalled();
+      expect(PackageService.prototype.prepareWebpack).not.toBeCalled();
+      expect(sls.cli.log).lastCalledWith("No need to perform webpack. Using pre-existing package");
+    });
+
+    it("does not call finalize if package specified in options", async () => {
+      await invokeHook(plugin, "after:package:finalize");
+      expect(PackageService.prototype.cleanUp).not.toBeCalled();
+      expect(sls.cli.log).lastCalledWith("No need to clean up generated folders & files. Using pre-existing package");
+    });
   })
 });
