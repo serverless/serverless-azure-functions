7a8a948d6ee8a1f9e21ebc478ba9605e5afdcdd3
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-08-16 07:38:32 -0700

    feat: Release and pre-release pipelines with publish script (#224)
    Pipelines for release and pre-release on merges to master and dev respectively. Dev branch will release a beta to NPM and Master will release a patch to NPM
    
    Resolves [AB#588]

---
+++ scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+$(pwd)/scripts/build.sh
+
+# NOTE: auth is taken care of via NPM_TOKEN env variable
+if [ -z "$1" ]; then
+  echo "Publishing 'latest' to NPM...";
+  npm publish
+else
+  echo "Publishing 'prerelease' to NPM...";
+  npm publish --tag=beta
+fi
---
+++ scripts/version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+# Environment Variables Needed:
+# SOURCE_BRANCH
+# GITHUB_ACCESS_TOKEN
+
+PACKAGE_NAME=$1
+NPM_RELEASE_TYPE=${2-"prerelease"}
+
+# Get full branch name excluding refs/head from the env var SOURCE_BRANCH
+SOURCE_BRANCH_NAME=${SOURCE_BRANCH/refs\/heads\/}
+
+# Configure git to commit as SLS Azure Functions Service Account
+echo "Configuring git to use service account..."
+git config --local user.email "Serverless Azure Functions"
+git config --local user.name "sls-az@microsoft.com"
+
+git pull origin ${SOURCE_BRANCH_NAME}
+git checkout ${SOURCE_BRANCH_NAME}
+echo "Checked out branch: ${SOURCE_BRANCH_NAME}"
+
+NPM_VERSION=`npm version ${NPM_RELEASE_TYPE} -m "release: Update ${NPM_RELEASE_TYPE} version to %s ***NO_CI***"`
+echo "Set NPM version to: ${NPM_VERSION}"
+
+SHA=`git rev-parse HEAD`
+
+git remote add authOrigin https://${GITHUB_ACCESS_TOKEN}@github.com/serverless/serverless-azure-functions.git
+git push authOrigin ${SOURCE_BRANCH_NAME} --tags
+
+echo "Pushed new tag: ${PACKAGE_NAME}-${NPM_VERSION} @ SHA: ${SHA:0:8}"
