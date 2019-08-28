401e74c8fd72a222f862c4ade801021d0e76a622
My <mydiemho@users.noreply.github.com>
2019-08-28 11:46:21 -0700

    fix: set up npm auth (#272)
    * create .npmrc on the fly

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,10 @@ set -eo pipefail
 
 $(pwd)/scripts/build.sh
 
+# set up .npmrc to authenticate with the provided token
+echo "Set up .npmrc ..."
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
 # NOTE: auth is taken care of via NPM_TOKEN env variable
 if [ -z "$1" ]; then
   echo "Publishing 'latest' to NPM...";
