d4e4fb80a783962248433d55aa1c21969154b30a
Wallace Breza <wallace@breza.me>
2019-09-05 08:47:49 -0700

    test: Verify default / custom named bindings

--- src/shared/binding.test.ts
+++ src/shared/binding.test.ts
@@ -31,4 +31,41 @@ describe("Bindings", () => {
       name: "res"
     });
   });
+
+  it("Gets the http binding with default settings", () => {
+    const serverless = MockFactory.createTestServerless();
+    const parsedBindings = BindingUtils.getBindingsMetaData(serverless);
+    const bindingType = "http";
+
+    const bindingTypes = parsedBindings.bindingTypes;
+    const bindingTypeIndex = bindingTypes.indexOf(bindingType);
+    const bindingSettings = parsedBindings.bindingSettings[bindingTypeIndex];
+
+    const binding = BindingUtils.getBinding(bindingType, bindingSettings, {});
+
+    expect(binding).toMatchObject({
+      type: "http",
+      direction: "out",
+      name: "res",
+    });
+  });
+
+  it("Gets the http binding with custom name", () => {
+    const serverless = MockFactory.createTestServerless();
+    const parsedBindings = BindingUtils.getBindingsMetaData(serverless);
+    const bindingType = "http";
+    const userSettings = { name: "custom" };
+
+    const bindingTypes = parsedBindings.bindingTypes;
+    const bindingTypeIndex = bindingTypes.indexOf(bindingType);
+    const bindingSettings = parsedBindings.bindingSettings[bindingTypeIndex];
+
+    const binding = BindingUtils.getBinding(bindingType, bindingSettings, userSettings);
+
+    expect(binding).toMatchObject({
+      type: "http",
+      direction: "out",
+      name: userSettings.name,
+    });
+  });
 });
