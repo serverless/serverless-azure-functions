1c3923c29358de6a5b13882ad6d8a15d44c5ab71
Wallace Breza <wallace@breza.me>
2019-10-25 15:26:50 -0700

    feat: Updated scaling support in premium app service plan (#386)
    Adds support to specify your scaling parameters for the app service plan hosting a premium functions app.

--- src/armTemplates/resources/appServicePlan.test.ts
+++ src/armTemplates/resources/appServicePlan.test.ts
@@ -52,4 +52,96 @@ describe("App Service Plan Resource", () => {
 
     expect(AppServicePlanResource.getResourceName(config)).toEqual(appServicePlanName);
   });
+
+  it("generates correct default parameters", () => {
+    const config: ServerlessAzureConfig = {
+      provider: {
+        name: "azure",
+        prefix,
+        region,
+        stage,
+        resourceGroup: resourceGroupName,
+        runtime: "nodejs10.x",
+      },
+      plugins: [],
+      functions: {},
+      service: "myapp",
+    };
+
+    const appServicePlanResource = new AppServicePlanResource();
+    const params = appServicePlanResource.getParameters(config);
+
+    expect(Object.keys(params)).toHaveLength(7);
+    expect(params.appServicePlanName.value).toEqual(AppServicePlanResource.getResourceName(config));
+    expect(params.appServicePlanSkuName.value).toBeUndefined();
+    expect(params.appServicePlanSkuTier.value).toBeUndefined();
+    expect(params.appServicePlanWorkerSizeId.value).toBeUndefined();
+    expect(params.appServicePlanMinWorkerCount.value).toBeUndefined();
+    expect(params.appServicePlanMaxWorkerCount.value).toBeUndefined();
+    expect(params.appServicePlanHostingEnvironment.value).toBeUndefined();
+  });
+
+  it("generates correct specified parameters", () => {
+    const config: ServerlessAzureConfig = {
+      provider: {
+        name: "azure",
+        prefix,
+        region,
+        stage,
+        resourceGroup: resourceGroupName,
+        runtime: "nodejs10.x",
+        appServicePlan: {
+          name: "customAppServicePlanName",
+          hostingEnvironment: "customHostingEnvironment",
+          sku: {
+            name: "EP2",
+            tier: "specialTier"
+          },
+          scale: {
+            minWorkerCount: 3,
+            maxWorkerCount: 20,
+            workerSizeId: "4"
+          }
+        }
+      },
+      plugins: [],
+      functions: {},
+      service: "myapp",
+    };
+
+    const appServicePlanResource = new AppServicePlanResource();
+    const params = appServicePlanResource.getParameters(config);
+
+    expect(Object.keys(params)).toHaveLength(7);
+    expect(params.appServicePlanName.value).toEqual(config.provider.appServicePlan.name);
+    expect(params.appServicePlanSkuName.value).toEqual(config.provider.appServicePlan.sku.name);
+    expect(params.appServicePlanSkuTier.value).toEqual(config.provider.appServicePlan.sku.tier);
+    expect(params.appServicePlanWorkerSizeId.value).toEqual(config.provider.appServicePlan.scale.workerSizeId);
+    expect(params.appServicePlanMinWorkerCount.value).toEqual(config.provider.appServicePlan.scale.minWorkerCount);
+    expect(params.appServicePlanMaxWorkerCount.value).toEqual(config.provider.appServicePlan.scale.maxWorkerCount);
+    expect(params.appServicePlanHostingEnvironment.value).toEqual(config.provider.appServicePlan.hostingEnvironment);
+  });
+
+  it("generates the expected template", () => {
+    const appServicePlanResource = new AppServicePlanResource();
+    const template = appServicePlanResource.getTemplate();
+    const appServicePlan = template.resources[0];
+
+    expect(appServicePlan).toMatchObject({
+      name: "[parameters('appServicePlanName')]",
+      type: "Microsoft.Web/serverfarms",
+      location: "[parameters('location')]",
+      properties: {
+        name: "[parameters('appServicePlanName')]",
+        workerSizeId: "[parameters('appServicePlanWorkerSizeId')]",
+        numberOfWorkers: "[parameters('appServicePlanMinWorkerCount')]",
+        maximumElasticWorkerCount: "[parameters('appServicePlanMaxWorkerCount')]",
+        hostingEnvironment: "[parameters('appServicePlanHostingEnvironment')]",
+      },
+      sku: {
+        name: "[parameters('appServicePlanSkuName')]",
+        tier: "[parameters('appServicePlanSkuTier')]"
+      }
+    });
+  });
 });
