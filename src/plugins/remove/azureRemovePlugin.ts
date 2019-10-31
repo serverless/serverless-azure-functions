f2e96a19a652608fa56b75bef3277227291991a4
Tanner Barlow <tanner.barlow.dev@gmail.com>
2019-10-31 13:55:36 -0700

    feat: Prompt user for confirmation to remove resource group (#383)

--- src/plugins/remove/azureRemovePlugin.ts
+++ src/plugins/remove/azureRemovePlugin.ts
@@ -1,6 +1,7 @@
 import Serverless from "serverless";
 import { ResourceService } from "../../services/resourceService";
 import { AzureBasePlugin } from "../azureBasePlugin";
+import { Utils } from "../../shared/utils";
 
 export class AzureRemovePlugin extends AzureBasePlugin {
 
@@ -40,10 +41,26 @@ export class AzureRemovePlugin extends AzureBasePlugin {
   }
 
   private async remove() {
-    const resourceClient = new ResourceService(this.serverless, this.options);
-    await resourceClient.deleteDeployment();
-    await resourceClient.deleteResourceGroup();
-
-    this.log("Service successfully removed");
+    const resourceService = new ResourceService(this.serverless, this.options);
+    const rg = await resourceService.getResourceGroup();
+    const rgName = resourceService.getResourceGroupName();
+    if (!rg) {
+      this.log(`Resource group "${rgName}" does not exist in your Azure subscription`)
+      return;
+    }
+    this.log(`This command will delete your ENTIRE resource group (${resourceService.getResourceGroupName()}). ` +
+      "and ALL the Azure resources that it contains " +
+      "Are you sure you want to proceed? If so, enter the full name of the resource group :");
+    const input = await Utils.waitForUserInput();
+    if (input === resourceService.getResourceGroupName()) {
+      this.log("Deleting resource group");
+      await resourceService.deleteDeployment();
+      await resourceService.deleteResourceGroup();
+      this.log("Service successfully removed");
+    }
+    else {
+      this.log("Will not remove resource group.");
+      return;
+    }
   }
 }
