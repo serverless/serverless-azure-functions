e0339ed0466c8baf09b2b8bcb555ff1fadd045d7
Tanner Barlow <tanner.barlow.dev@gmail.com>
2020-03-09 09:42:19 -0700

    feat: Logging Service to allow configurable verbosity (#401)

--- src/plugins/azureBasePlugin.test.ts
+++ src/plugins/azureBasePlugin.test.ts
@@ -2,6 +2,9 @@ import { AzureBasePlugin } from "./azureBasePlugin";
 import Serverless from "serverless";
 import { MockFactory } from "../test/mockFactory";
 
+jest.mock("../services/loggingService");
+import { LoggingService } from "../services/loggingService";
+
 class MockPlugin extends AzureBasePlugin {
   public constructor(serverless: Serverless, options: Serverless.Options) {
     super(serverless, options);
@@ -13,6 +16,22 @@ class MockPlugin extends AzureBasePlugin {
 
   public getServerlessOption(key: string, defaultValue?: any) {
     return this.getOption(key, defaultValue);
+  }
+
+  public err(message: string) {
+    this.error(message);
+  }
+
+  public war(message: string) {
+    this.warn(message);
+  }
+
+  public inf(message: string) {
+    this.info(message);
+  }
+
+  public deb(message: string) {
+    this.debug(message);
   }
 }
 
@@ -31,7 +50,7 @@ describe("Azure Base Plugin", () => {
 
   it("logs a message", () => {
     plugin.logMessage("test message");
-    expect(sls.cli.log).lastCalledWith("test message");
+    expect(LoggingService.prototype.log).lastCalledWith("test message", undefined);
   });
 
   it("gets an option", () => {
@@ -39,5 +58,21 @@ describe("Azure Base Plugin", () => {
     expect(plugin.getServerlessOption("key2")).toEqual("val2");
     expect(plugin.getServerlessOption("key3")).toBeUndefined();
     expect(plugin.getServerlessOption("key3", "myValue")).toEqual("myValue");
+  });
+
+  it("calls LoggingService", () => {
+    const mockPlugin = new MockPlugin(sls, {} as any);
+    
+    mockPlugin.err("error");
+    expect(LoggingService.prototype.error).toBeCalledWith("error");
+
+    mockPlugin.war("warn");
+    expect(LoggingService.prototype.warn).toBeCalledWith("warn");
+
+    mockPlugin.inf("info");
+    expect(LoggingService.prototype.info).toBeCalledWith("info");
+
+    mockPlugin.deb("debug");
+    expect(LoggingService.prototype.debug).toBeCalledWith("debug");
   });
 })
--- src/services/baseService.test.ts
+++ src/services/baseService.test.ts
@@ -5,6 +5,9 @@ import { ServerlessAzureOptions } from "../models/serverless";
 import { MockFactory } from "../test/mockFactory";
 import { BaseService } from "./baseService";
 
+jest.mock("./loggingService");
+import { LoggingService, LogLevel } from "./loggingService";
+
 jest.mock("axios", () => jest.fn());
 import axios from "axios";
 
@@ -33,6 +36,22 @@ class MockService extends BaseService {
       resourceGroup: this.resourceGroup,
       deploymentName: this.deploymentName,
     };
+  }
+
+  public err(message: string) {
+    this.error(message);
+  }
+
+  public war(message: string) {
+    this.warn(message);
+  }
+
+  public inf(message: string) {
+    this.info(message);
+  }
+
+  public deb(message: string) {
+    this.debug(message);
   }
 }
 
@@ -140,5 +159,21 @@ describe("Base Service", () => {
     expect(request).toBeCalledWith(requestOptions, expect.anything());
 
     readStreamSpy.mockRestore();
+  });
+
+  it("calls LoggingService", () => {
+    const mockService = new MockService(serverless);
+    
+    mockService.err("error");
+    expect(LoggingService.prototype.error).toBeCalledWith("error");
+
+    mockService.war("warn");
+    expect(LoggingService.prototype.warn).toBeCalledWith("warn");
+
+    mockService.inf("info");
+    expect(LoggingService.prototype.info).toBeCalledWith("info");
+
+    mockService.deb("debug");
+    expect(LoggingService.prototype.debug).toBeCalledWith("debug");
   });
 });
