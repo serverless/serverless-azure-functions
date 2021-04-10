258105311a65acc4e82901a099a90d1b050c9512
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-04-10 13:25:56 -0300

    test: accept nodejs14 on runtime

--- src/config/runtime.test.ts
+++ src/config/runtime.test.ts
@@ -5,6 +5,7 @@ describe("Runtime", () => {
   it("identifies node runtimes correctly", () => {
     expect(isNodeRuntime(Runtime.NODE10)).toBe(true);
     expect(isNodeRuntime(Runtime.NODE12)).toBe(true);
+    expect(isNodeRuntime(Runtime.NODE14)).toBe(true);
     expect(isNodeRuntime(Runtime.PYTHON36)).toBe(false);
     expect(isNodeRuntime(Runtime.PYTHON37)).toBe(false);
     expect(isNodeRuntime(Runtime.PYTHON38)).toBe(false);
@@ -13,6 +14,7 @@ describe("Runtime", () => {
   it("identifies python runtimes correctly", () => {
     expect(isPythonRuntime(Runtime.NODE10)).toBe(false);
     expect(isPythonRuntime(Runtime.NODE12)).toBe(false);
+    expect(isPythonRuntime(Runtime.NODE14)).toBe(false);
     expect(isPythonRuntime(Runtime.PYTHON36)).toBe(true);
     expect(isPythonRuntime(Runtime.PYTHON37)).toBe(true);
     expect(isPythonRuntime(Runtime.PYTHON38)).toBe(true);
@@ -21,6 +23,7 @@ describe("Runtime", () => {
   it("gets runtime version", () => {
     expect(getRuntimeVersion(Runtime.NODE10)).toBe("10");
     expect(getRuntimeVersion(Runtime.NODE12)).toBe("12");
+    expect(getRuntimeVersion(Runtime.NODE14)).toBe("14");
     expect(getRuntimeVersion(Runtime.PYTHON36)).toBe("3.6");
     expect(getRuntimeVersion(Runtime.PYTHON37)).toBe("3.7");
     expect(getRuntimeVersion(Runtime.PYTHON38)).toBe("3.8");
@@ -29,6 +32,7 @@ describe("Runtime", () => {
   it("gets runtime language", () => {
     expect(getRuntimeLanguage(Runtime.NODE10)).toBe("nodejs");
     expect(getRuntimeLanguage(Runtime.NODE12)).toBe("nodejs");
+    expect(getRuntimeLanguage(Runtime.NODE14)).toBe("nodejs");
     expect(getRuntimeLanguage(Runtime.PYTHON36)).toBe("python");
     expect(getRuntimeLanguage(Runtime.PYTHON37)).toBe("python");
     expect(getRuntimeLanguage(Runtime.PYTHON38)).toBe("python");
@@ -37,6 +41,7 @@ describe("Runtime", () => {
   it("gets function worker runtime", () => {
     expect(getFunctionWorkerRuntime(Runtime.NODE10)).toBe("node");
     expect(getFunctionWorkerRuntime(Runtime.NODE12)).toBe("node");
+    expect(getFunctionWorkerRuntime(Runtime.NODE14)).toBe("node");
     expect(getFunctionWorkerRuntime(Runtime.PYTHON36)).toBe("python");
     expect(getFunctionWorkerRuntime(Runtime.PYTHON37)).toBe("python");
     expect(getFunctionWorkerRuntime(Runtime.PYTHON38)).toBe("python");
