eb9a88e04ec046bcdda0364e6cfd7c0cd9ae77e9
Lucas Nunes <lucas.nunes@cobasi.com.br>
2023-02-02 13:03:51 -0300

    test: including node16 like a valid runtime

--- src/config/runtime.test.ts
+++ src/config/runtime.test.ts
@@ -1,11 +1,11 @@
 import { Runtime, isNodeRuntime, isPythonRuntime, getRuntimeVersion, getRuntimeLanguage, getFunctionWorkerRuntime } from "./runtime";
 
-
 describe("Runtime", () => {
   it("identifies node runtimes correctly", () => {
     expect(isNodeRuntime(Runtime.NODE10)).toBe(true);
     expect(isNodeRuntime(Runtime.NODE12)).toBe(true);
     expect(isNodeRuntime(Runtime.NODE14)).toBe(true);
+    expect(isNodeRuntime(Runtime.NODE16)).toBe(true);
     expect(isNodeRuntime(Runtime.PYTHON36)).toBe(false);
     expect(isNodeRuntime(Runtime.PYTHON37)).toBe(false);
     expect(isNodeRuntime(Runtime.PYTHON38)).toBe(false);
@@ -15,6 +15,7 @@ describe("Runtime", () => {
     expect(isPythonRuntime(Runtime.NODE10)).toBe(false);
     expect(isPythonRuntime(Runtime.NODE12)).toBe(false);
     expect(isPythonRuntime(Runtime.NODE14)).toBe(false);
+    expect(isPythonRuntime(Runtime.NODE16)).toBe(false);
     expect(isPythonRuntime(Runtime.PYTHON36)).toBe(true);
     expect(isPythonRuntime(Runtime.PYTHON37)).toBe(true);
     expect(isPythonRuntime(Runtime.PYTHON38)).toBe(true);
@@ -24,24 +25,39 @@ describe("Runtime", () => {
     expect(getRuntimeVersion(Runtime.NODE10)).toBe("10");
     expect(getRuntimeVersion(Runtime.NODE12)).toBe("12");
     expect(getRuntimeVersion(Runtime.NODE14)).toBe("14");
+    expect(getRuntimeVersion(Runtime.NODE16)).toBe("16");
     expect(getRuntimeVersion(Runtime.PYTHON36)).toBe("3.6");
     expect(getRuntimeVersion(Runtime.PYTHON37)).toBe("3.7");
     expect(getRuntimeVersion(Runtime.PYTHON38)).toBe("3.8");
   });
 
+  it("throw exception on get runtime version", () => {
+    Runtime["invalid"] = "invalid"
+    expect(() => { getRuntimeVersion(Runtime["invalid"]) }).toThrowError("Runtime invalid not included in supportedRuntimes")
+    delete Runtime["invalid"]
+  })
+
   it("gets runtime language", () => {
     expect(getRuntimeLanguage(Runtime.NODE10)).toBe("nodejs");
     expect(getRuntimeLanguage(Runtime.NODE12)).toBe("nodejs");
     expect(getRuntimeLanguage(Runtime.NODE14)).toBe("nodejs");
+    expect(getRuntimeLanguage(Runtime.NODE16)).toBe("nodejs");
     expect(getRuntimeLanguage(Runtime.PYTHON36)).toBe("python");
     expect(getRuntimeLanguage(Runtime.PYTHON37)).toBe("python");
     expect(getRuntimeLanguage(Runtime.PYTHON38)).toBe("python");
   });
 
+  it("throw exception on get runtime language", () => {
+    Runtime["invalid"] = "invalid"
+    expect(() => { getRuntimeLanguage(Runtime["invalid"]) }).toThrowError("Runtime invalid not included in supportedRuntimes")
+    delete Runtime["invalid"]
+  })
+
   it("gets function worker runtime", () => {
     expect(getFunctionWorkerRuntime(Runtime.NODE10)).toBe("node");
     expect(getFunctionWorkerRuntime(Runtime.NODE12)).toBe("node");
     expect(getFunctionWorkerRuntime(Runtime.NODE14)).toBe("node");
+    expect(getFunctionWorkerRuntime(Runtime.NODE16)).toBe("node");
     expect(getFunctionWorkerRuntime(Runtime.PYTHON36)).toBe("python");
     expect(getFunctionWorkerRuntime(Runtime.PYTHON37)).toBe("python");
     expect(getFunctionWorkerRuntime(Runtime.PYTHON38)).toBe("python");
