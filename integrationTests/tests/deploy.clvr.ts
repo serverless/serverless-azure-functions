3dd4fb17666546b64fe0ef73ac3e017e40c94b54
Gligor Kotushevski <gligorkot@gmail.com>
2023-03-01 23:20:44 +1300

    remove deploy/invoke/remove integration tests as azure credentials don't work anymore for the parent repo

--- integrationTests/tests/deploy.clvr.ts
+++ integrationTests/tests/deploy.clvr.ts
@@ -12,52 +12,52 @@ run({
     {
       command: "sls package",
     },
-    {
-      command: "sls deploy -p .serverless/${serviceName}.zip",
-      retries: 0,
-      stdout: {
-        shouldContain: [
-          "Logging into Azure",
-          "Deployed serverless functions:",
-          "int-${region}-qa-${serviceName}-rg",
-          "-> hello: [GET] int-${region}-qa-${serviceName}.azurewebsites.net/api/hello"
-        ],
-      }
-    },
-    {
-      command: `sls invoke -f hello -d ${JSON.stringify({name: "Azure"}).replace(" ", "")}`,
-      retries: 0,
-      stdout: {
-        shouldContain: [
-          "Hello Azure ${runtime}",
-          "Environment variable: foo"
-        ]
-      },
-      stderr: {
-        shouldBeExactly: ""
-      }
-    },
-    {
-      command: `sls invoke apim -f hello -d ${JSON.stringify({name: "Azure"}).replace(" ", "")}`,
-      retries: 0,
-      stdout: {
-        shouldContain: [
-          "Hello Azure ${runtime}",
-          "Environment variable: foo"
-        ]
-      },
-      stderr: {
-        shouldBeExactly: ""
-      },
-    },
-    {
-      command: "sls remove --force",
-      retries: 0,
-      stdout: {
-        shouldContain: [
-          "Service successfully removed"
-        ]
-      }
-    },
+    // {
+    //   command: "sls deploy -p .serverless/${serviceName}.zip",
+    //   retries: 0,
+    //   stdout: {
+    //     shouldContain: [
+    //       "Logging into Azure",
+    //       "Deployed serverless functions:",
+    //       "int-${region}-qa-${serviceName}-rg",
+    //       "-> hello: [GET] int-${region}-qa-${serviceName}.azurewebsites.net/api/hello"
+    //     ],
+    //   }
+    // },
+    // {
+    //   command: `sls invoke -f hello -d ${JSON.stringify({name: "Azure"}).replace(" ", "")}`,
+    //   retries: 0,
+    //   stdout: {
+    //     shouldContain: [
+    //       "Hello Azure ${runtime}",
+    //       "Environment variable: foo"
+    //     ]
+    //   },
+    //   stderr: {
+    //     shouldBeExactly: ""
+    //   }
+    // },
+    // {
+    //   command: `sls invoke apim -f hello -d ${JSON.stringify({name: "Azure"}).replace(" ", "")}`,
+    //   retries: 0,
+    //   stdout: {
+    //     shouldContain: [
+    //       "Hello Azure ${runtime}",
+    //       "Environment variable: foo"
+    //     ]
+    //   },
+    //   stderr: {
+    //     shouldBeExactly: ""
+    //   },
+    // },
+    // {
+    //   command: "sls remove --force",
+    //   retries: 0,
+    //   stdout: {
+    //     shouldContain: [
+    //       "Service successfully removed"
+    //     ]
+    //   }
+    // },
   ]
 });
