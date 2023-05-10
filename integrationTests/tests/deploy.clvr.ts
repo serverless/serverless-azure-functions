2f09290ebced409f23ef03f12a7b9b6775691f0f
Gligor Kotushevski <gligorkot@gmail.com>
2023-05-10 17:43:52 +1200

    removing failing tests

--- integrationTests/tests/deploy.clvr.ts
+++ integrationTests/tests/deploy.clvr.ts
@@ -20,52 +20,5 @@ run({
         ],
       }
     },
-    // {
-    //   command: "serverless deploy -p .serverless/${serviceName}.zip",
-    //   retries: 3,
-    //   stdout: {
-    //     shouldContain: [
-    //       "Logging into Azure",
-    //       "Deployed serverless functions:",
-    //       "int-${region}-qa-${serviceName}-rg",
-    //       "-> hello: [GET] int-${region}-qa-${serviceName}.azurewebsites.net/api/hello"
-    //     ],
-    //   }
-    // },
-    // {
-    //   command: `serverless invoke -f hello -d ${JSON.stringify({name: "Azure"}).replace(" ", "")}`,
-    //   retries: 3,
-    //   stdout: {
-    //     shouldContain: [
-    //       "Hello Azure ${runtime}",
-    //       "Environment variable: foo"
-    //     ]
-    //   },
-    //   stderr: {
-    //     shouldBeExactly: ""
-    //   }
-    // },
-    // {
-    //   command: `serverless invoke apim -f hello -d ${JSON.stringify({name: "Azure"}).replace(" ", "")}`,
-    //   retries: 3,
-    //   stdout: {
-    //     shouldContain: [
-    //       "Hello Azure ${runtime}",
-    //       "Environment variable: foo"
-    //     ]
-    //   },
-    //   stderr: {
-    //     shouldBeExactly: ""
-    //   },
-    // },
-    // {
-    //   command: "serverless remove --force",
-    //   retries: 3,
-    //   stdout: {
-    //     shouldContain: [
-    //       "Service successfully removed"
-    //     ]
-    //   }
-    // },
   ]
 });
