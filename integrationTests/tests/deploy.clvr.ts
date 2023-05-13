21c7b30c0b2128168da0b10a95aa92cfe7f9fbab
Gligor Kotushevski <gligorkot@gmail.com>
2023-05-13 14:55:28 +1200

    adding dotnet tests and removing last failing test + getting integration tests to run validate steps

--- integrationTests/tests/deploy.clvr.ts
+++ integrationTests/tests/deploy.clvr.ts
@@ -2,23 +2,13 @@ import run from "clvr";
 import { defaultParameters } from "../parameters";
 
 run({
-  name: "Deploy Test",
+  name: "Install Test",
   parameters: defaultParameters,
   validations: [
     {
       // relative path to package.json so that we run the rest of the tests using the updated package
       command: "npm i ../../../",
       silent: true,
-    },
-    {
-      command: "serverless package",
-      stdout: {
-        shouldContain: [
-          "Serverless: Initializing provider configuration...",
-          "Serverless: Parsing Azure Functions Bindings.json...",
-          "Serverless: Building binding for function: hello event: httpTrigger",
-        ],
-      }
     },
   ]
 });
