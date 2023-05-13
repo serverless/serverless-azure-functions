55e891cf37604fbb25151855352319484bb70610
Gligor Kotushevski <gligorkot@gmail.com>
2023-05-13 15:16:07 +1200

    bring back package test - make sure to run npm pack before running integration tests

--- integrationTests/tests/deploy.clvr.ts
+++ integrationTests/tests/deploy.clvr.ts
@@ -2,13 +2,23 @@ import run from "clvr";
 import { defaultParameters } from "../parameters";
 
 run({
-  name: "Install Test",
+  name: "Package Tests",
   parameters: defaultParameters,
   validations: [
     {
       // relative path to package.json so that we run the rest of the tests using the updated package
       command: "npm i ../../../",
       silent: true,
+    },
+    {
+      command: "serverless package",
+      stdout: {
+        shouldContain: [
+          "Serverless: Initializing provider configuration...",
+          "Serverless: Parsing Azure Functions Bindings.json...",
+          "Serverless: Building binding for function: hello event: httpTrigger",
+        ],
+      }
     },
   ]
 });
